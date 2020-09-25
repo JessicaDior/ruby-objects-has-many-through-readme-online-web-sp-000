228f1f090df82c7b5e2495e868a4a57a44ddbef2
Jc Dior <jessicaclairdior@gmail.com>
2020-09-25 04:02:27 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -23,11 +23,6 @@ class Customer
     end
   end
 
-  def meals
-  Meal.all.select do |meal|
-    meal.waiter == self #checking for waiter now
-  end
-end
 
 def best_tipper
   best_tipped_meal = meals.max do |meal_a, meal_b|
